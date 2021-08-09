a21116bcf2469149168bf7b9bb05fc17790a6528
Dhiraj Arun <dhrjarun@gmail.com>
2021-08-09 17:32:02 +0530

    replace jwtPrivateKey from config with JWTKEY of enviroment variable

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,14 @@
-const jwt = require('jsonwebtoken');
-const config = require('config')
+const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-    const token = req.header('x-auth-token')
-    if(!token) return res.status(401).send("Access denied, no token provided")
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).send("Access denied, no token provided");
 
-   try{
-    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+  try {
+    const decoded = jwt.verify(token, process.env.JWTKEY);
     req.user = decoded;
     next();
-   }
-   catch(ex){
-       res.status(400).send("invalid token")
-
-   }
-}
+  } catch (ex) {
+    res.status(400).send("invalid token");
+  }
+};
--- models/user.js
+++ models/user.js
@@ -1,56 +1,57 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
-const jwt = require("jsonwebtoken")
-const _ = require('lodash')
-const config = require('config')
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        maxlenght: 50
-    },
-    email: {
-        type: String,
-        minlength: 5,
-        maxlength: 255,
-        unique: true,
-        sparse: true
-    },
-    phone: {
-        type: String,
-        match: /[\d]{10}/,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 1024
-    }
+  name: {
+    type: String,
+    required: true,
+    maxlenght: 50,
+  },
+  email: {
+    type: String,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+    sparse: true,
+  },
+  phone: {
+    type: String,
+    match: /[\d]{10}/,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 1024,
+  },
+});
 
-})
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    _.pick(this, ["_id", "name", "email", "phone"]),
+    process.env.JWTKEY
+  );
+  return token;
+};
 
-userSchema.methods.generateAuthToken = function(){
-    const token = jwt.sign(_.pick(this,['_id','name', 'email', 'phone']), config.get('jwtPrivateKey'))
-    return token;
-}
-
-function validateUser(user){
-    const schema = Joi.object({
-        name: Joi.string().max(50).required(),
-        email: Joi.string().min(5).max(255).email(),
-        phone: Joi.string().max(10).pattern(/[\d]{10}/).required(),
-        password: Joi.string().min(6).max(1024).required(),
-    })
-    return schema.validate(user);
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().max(50).required(),
+    email: Joi.string().min(5).max(255).email(),
+    phone: Joi.string()
+      .max(10)
+      .pattern(/[\d]{10}/)
+      .required(),
+    password: Joi.string().min(6).max(1024).required(),
+  });
+  return schema.validate(user);
 }
 
 const User = mongoose.model("User", userSchema);
 
-
 exports.User = User;
 exports.validateUser = validateUser;
-
-
