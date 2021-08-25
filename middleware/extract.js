91d374f6add2876380e137ff6eb6e64fff9de27c
Dhiraj Arun <dhrjarun@gmail.com>
2021-08-25 17:46:05 +0530

    extracted the sharp.js into extract.js and thumbnail.js

--- middleware/extract.js
+++ middleware/extract.js
@@ -1,7 +1,39 @@
-const sharp = require("../middleware/sharp");
+const sharp = require("sharp");
+const { join } = require("path");
+const sizeOf = require("image-size");
 const { getName, getExt } = require("../utils/fileName");
 
-module.exports = sharp.extract(null, {
+const extract = (data, { source, dest, fileName }) => {
+  return async (req, res, next) => {
+    if (data == null)
+      data = {
+        width: parseInt(req.body.width),
+        height: parseInt(req.body.height),
+        top: parseInt(req.body.top),
+        left: parseInt(req.body.left),
+      };
+
+    const path = source(req);
+    const filename = fileName(req, data);
+    const newPath = join(dest(req), filename);
+
+    const sourceDimens = sizeOf(path);
+
+    if (sourceDimens.width < data.width || sourceDimens.height < data.width) {
+      return res.status(400).send("width and height of image should be more");
+    }
+
+    try {
+      const value = await sharp(path).extract(data).toFile(newPath);
+      req.extracted = { filename, path: newPath, ...value };
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+module.exports = extract(null, {
   source: (req) => {
     return req.file.path;
   },
--- middleware/thumbnails.js
+++ middleware/thumbnails.js
@@ -1,5 +1,41 @@
-const sharp = require("../middleware/sharp");
+const sharp = require("sharp");
 const { getName, getExt } = require("../utils/fileName");
+const { join } = require("path");
+
+const createThumbnails = (input, { source, dest, fileName }) => {
+  return async (req, res, next) => {
+    let data;
+    if (typeof input == "function") {
+      data = input(req);
+    } else {
+      data = input;
+    }
+
+    const path = source(req);
+    let thumbnails = [];
+
+    data.forEach((item, index) => {
+      const filename = fileName(req, item);
+      const newPath = join(dest(req), filename);
+
+      thumbnails[index] = new Promise(async (resolve, reject) => {
+        try {
+          const value = await sharp(path).resize(item).toFile(newPath);
+          resolve({ ...value, filename, path: newPath });
+        } catch (err) {
+          reject(err.message);
+        }
+      });
+    });
+
+    try {
+      req.thumbnails = await Promise.all(thumbnails);
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
 
 const productThumbnails = [
   { width: 720, height: 200 },
@@ -10,7 +46,7 @@ const getThumbnails = (req) => {
   if (req.params.dest === "product") return productThumbnails;
 };
 
-module.exports = sharp.createThumbnails(getThumbnails, {
+module.exports = createThumbnails(getThumbnails, {
   source: (req) => {
     return req.extracted.path;
   },
