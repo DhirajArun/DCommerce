cb4c579b366dcad02d11c5c17f93b228c1026fd1
Dhiraj Arun <dhrjarun@gmail.com>
2021-08-31 16:05:00 +0530

    fixed another type of JWTKEY to JWT_KEY

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
   if (!token) return res.status(401).send("Access denied, no token provided");
 
   try {
-    const decoded = jwt.verify(token, process.env.JWTKEY);
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.user = decoded;
     next();
   } catch (ex) {
